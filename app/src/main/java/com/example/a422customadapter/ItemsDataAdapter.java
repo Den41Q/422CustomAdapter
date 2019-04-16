daa040969035bd96784c7676d91d0ec35c234959
Den41Q <226001011501ve>
2019-04-16 15:32:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a422customadapter/ItemsDataAdapter.java
@@ -0,0 +1,88 @@
+package com.example.a422customadapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemsDataAdapter extends BaseAdapter {
+
+    private List<ItemData> items;
+    private LayoutInflater inflater;
+
+    ItemsDataAdapter(Context context, List<ItemData> items) {
+        if (items == null) {
+            this.items = new ArrayList<>();
+        } else {
+            this.items = items;
+        }
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    void addItem(ItemData item) {
+        this.items.add(item);
+        notifyDataSetChanged();
+    }
+
+
+    void removeItem(int position) {
+        items.remove(position);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public ItemData getItem(int position) {
+        if (position < items.size()) {
+            return items.get(position);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.item_list_view, parent, false);
+        }
+
+        ItemData itemData = items.get(position);
+
+        ImageView image = view.findViewById(R.id.icon);
+        TextView title = view.findViewById(R.id.title);
+        TextView subtitle = view.findViewById(R.id.subtitle);
+        Button button = view.findViewById(R.id.button);
+
+        image.setImageDrawable(itemData.getImage());
+        title.setText(itemData.getTitle());
+        subtitle.setText(itemData.getSubtitle());
+        button.setTag(position);
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeItem(position);
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/a422customadapter/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.a422customadapter;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Random random = new Random();
+    private ItemsDataAdapter adapter;
+    private List<Drawable> images = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        ListView listView = findViewById(R.id.listView);
+
+        setSupportActionBar(toolbar);
+
+        fillImages();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateRandomItemData();
+            }
+        });
+
+        adapter = new ItemsDataAdapter(this, null);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                showItemData(position);
+                return true;
+            }
+        });
+    }
+
+    private void fillImages() {
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                R.drawable.ic_head_venom));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                R.drawable.ic_head_iron_man));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                R.drawable.ic_head_punisher));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                R.drawable.ic_head_spider_man));
+        images.add(ContextCompat.getDrawable(MainActivity.this,
+                R.drawable.ic_head_capitan_america));
+    }
+
+    private void generateRandomItemData() {
+        adapter.addItem(new ItemData(
+                images.get(random.nextInt(images.size())),
+                "Hello" + adapter.getCount(),
+                "It\'s me",
+                random.nextBoolean()));
+    }
+
+    private void showItemData(int position) {
+        ItemData itemData = adapter.getItem(position);
+        Toast.makeText(MainActivity.this,
+                "Title: " + itemData.getTitle() + "\n" +
+                        "Subtitle: " + itemData.getSubtitle(),
+                Toast.LENGTH_SHORT).show();
+    }
+}
